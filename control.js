5b9c9b6e12c9d8314ebfc30dae0bcff8a72b3775
tylercschneider <tylercschneider@gmail.com>
2018-05-19 15:36:40 -0400

    functioning

--- control.js
+++ control.js
@@ -2,18 +2,32 @@
 
 {
 	function todoController() {
-		let vm = this;
-		vm.todoList = [
-			{"id": 1, "task": "stuff to do 1", "complete": false},
-			{"id": 2, "task": "stuff to do 1", "complete": false},
-			{"id": 3, "task": "stuff to do 1", "complete": false},
-			{"id": 4, "task": "stuff to do 1", "complete": false},
-			{"id": 5, "task": "stuff to do 1", "complete": true},
-			{"id": 6, "task": "stuff to do 1", "complete": false},
-			{"id": 7, "task": "stuff to do 1", "complete": false},
-			{"id": 8, "task": "stuff to do 1", "complete": false},
-			{"id": 9,"task": "stuff to do 2", "complete": true}
+		let toDoList = this;
+		toDoList.todos = [
+			{"id": 0, "task": "stuff to do 1", "complete": true},
+			{"id": 1, "task": "fight", "complete": false},
+			{"id": 2, "task": "eat", "complete": true},
+			{"id": 3, "task": "program", "complete": false},
+			{"id": 4, "task": "learn", "complete": true},
+			{"id": 5, "task": "sleep", "complete": false},
+			{"id": 6, "task": "something", "complete": false},
+			{"id": 7, "task": "words", "complete": false},
+			{"id": 8,"task": "stuff to do 2", "complete": true}
 		];
+		toDoList.setDone = function(id) {
+			toDoList.todos[id].complete = true;
+		}
+		toDoList.remove = function(id) {
+			toDoList.todos.splice(id, 1);
+			for(let i=0; i<toDoList.todos.length; i++){
+				toDoList.todos[i].id = i;
+			}
+		}
+		// toDoList.add() = function() {
+		// 	let item = {
+		// 		"id": id,
+		// 	}
+		// }
 	}
 	angular
 	.module("app")
